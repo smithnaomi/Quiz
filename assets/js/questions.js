86a6bb32dc3ac4ace99642cc5a6d1d223f2acf1f
smithnaomi <88051105+smithnaomi@users.noreply.github.com>
2021-10-27 20:29:50 -0400

    array in an object

---
+++ assets/js/questions.js
@@ -0,0 +1,8 @@
+var questions = [
+    {
+        title: 'What types of information can variables store?',
+        choices: ['string','numbers', 'arrays','all of the above'],
+        answer: 'all of the above'
+    },
+    
+]
---
+++ assets/js/scrpit.js
@@ -0,0 +1,15 @@
+var currentquestionIndex = 0;
+var time = questions.length * 15;
+var timer ;
+
+var questionsElements = document.getElementById('questions')
+
+var timeElements = document.getElementById('time')
+
+var choicesElements = document.getElementById('choices')
+
+var submitBtn = document.getElementById('submit')
+
+var startBtn = document.getElementById('start')
+
+var initialsElement = document.getElementById('initials')
