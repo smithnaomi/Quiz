3222734ba3828ac4e5ddde8b460f243f5333f060
smithnaomi <88051105+smithnaomi@users.noreply.github.com>
2021-10-29 19:32:17 -0400

    setAtrribute,p tag, functions

--- assets/js/scrpit.js
+++ assets/js/scrpit.js
@@ -13,3 +13,39 @@ var submitBtn = document.getElementById('submit')
 var startBtn = document.getElementById('start')
 
 var initialsElement = document.getElementById('initials')
+
+
+function start (){
+var startscreenElement = document.getElementById('start-screen')
+startscreenElement.setAttribute('class','hide')
+
+questionsElements.removeAttribute('class')
+
+timer = setInterval(clock,1000)
+
+timeElements.textContent= time
+
+getnextquestion() 
+
+}
+function getnextquestion (){
+var currentQuestions= questions[currentquestionIndex]
+var questionsTitle = document.getElementById('questions-title')
+questionsTitle.textContent= currentQuestions.title
+choicesElements.innerHTML= ''
+currentQuestions.choices.forEach(function(choice,i){
+    var choicesData= document.createElement('button')
+    choicesData.setAttribute('class','choice')
+    choicesData.setAttribute('value',choice)
+choicesData.textContent= i+1+' . '+ choice
+choicesData.onclick= questionsclick
+choicesElements.appendChild(choicesData)
+}) 
+}
+
+function questionsclick(){
+
+
+}
+
+
